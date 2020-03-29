2e121a877eb31d23b748eb6d470b704706d11927
sduplacy <sduplacy2@gmail.com>
2020-03-29 13:39:53 -0400

    Index page - post route to submit email

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,9 @@
 exports.get_main = function(req, res, next) {
     res.render('index', { title: 'Express' });
   }
+
+exports.submit_lead = function (req, res , next){
+    console.log("Email", req.body.lead_email);
+    res.redirect('/');
+
+ } 
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ var router = express.Router();
 let index = require ('../controllers/index');
 /* GET home page. */
 router.get('/', index.get_main);
+router.post('/', index.submit_lead);
 
 module.exports = router;
